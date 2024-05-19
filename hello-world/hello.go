aa74e742cb9c13ec1a14cf6b6e82afa946bc5338
Javier <ingenierodesoftware73@gmail.com>
2024-05-19 17:59:44 -0500

    variables

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -8,4 +8,14 @@ import (
 func main(){
 	fmt.Println("Hello world")
 	fmt.Println(quote.Go())
+
+	// var firstName, lastName string
+	// var age int
+
+	firstName, lastName, age  := "Alex", "Roel", 27
+	age = 30
+	
+
+
+	fmt.Println(firstName, lastName, age)
 }
