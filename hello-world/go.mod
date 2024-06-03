709f6f2decc408d7b98c34a412a164044894fb83
Javier <ingenierodesoftware73@gmail.com>
2024-06-03 15:43:23 -0500

    relearning hello go

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,6 +1,6 @@
 module helloworld
 
-go 1.22.3
+go 1.20
 
 require (
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,38 +3,9 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
-) 	
-const Pi  = 3.14
-const (
-	x = 100
-	y = 0b1010
-	z = 0o12
-	w = 0xFF
 )
-const(
-	Sunday = iota + 1
-	Monday
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-	
-)
+
 func main(){
 	fmt.Println("Hello world")
 	fmt.Println(quote.Go())
-
-	// var firstName, lastName string
-	// var age int
-
-	firstName, lastName, age  := "Alex", "Roel", 27
-	age = 30
-	
-
-
-	fmt.Println(firstName, lastName, age)
-	fmt.Println(Pi)
-	fmt.Println(x, y, z, w)
-	fmt.Println(Sunday)
 }
